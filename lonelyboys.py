0ff8a61c7442cd811e4637147bd08ebf387b3ebe
lonelyboys11 <87362040+lonelyboys11@users.noreply.github.com>
2021-07-13 19:19:29 +1000

    Update lonelyboys.py

--- lonelyboys.py
+++ lonelyboys.py
@@ -1,20 +1,24 @@
-#!/usr/bin/env python
-
-# RENAME THIS FILE WITH YOUR TEAM NAME.
+#!/usr/bin/envblah python
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 nInst=100
 currentPos = np.zeros(nInst)
 
-# Dummy algorithm to demonstrate function format.
-def getMyPosition (prcSoFar):
+def getMyPosition(prcSoFar, n):
     global currentPos
     (nins,nt) = prcSoFar.shape
-    rpos = np.array([int(x) for x in 1000 * np.random.randn(nins)])
-    currentPos += rpos
+    currentPos = np.zeros(100)
+
+    new = prcSoFar / np.roll(prcSoFar, (1, 0), axis=1)
+    new[:, 0] = np.ones(shape=new[:, 0].shape)
+    price_move = np.log(new)
+
+    currentPos[0:50] = np.where(np.log(prcSoFar[0:50, -1]/prcSoFar[0:50, 0])/nt > np.log(1 + 0.00075), 10000000000, np.where(np.log(prcSoFar[0:50, -1]/prcSoFar[0:50, 0])/nt < np.log(1 - 0.00075), -10000000, 0))
+    currentPos[50:100] = np.where(price_move[50:100, -1] > np.log(1.00), -10000000, np.where(price_move[50:100, -1] < np.log(1.0), 10000000, 0))
+
     # The algorithm must return a vector of integers, indicating the position of each stock.
     # Position = number of shares, and can be positve or negative depending on long/short position.
     return currentPos
-
-    
